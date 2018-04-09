723ba0f58580fe02f858ac2342847ed1be778e6d
mengrain <l>
2018-04-09 10:45:25 +0800

    Signed-off-by: mengrain <l>

---
+++ node/app.js
@@ -0,0 +1,97 @@
+var express = require('express')
+var bodyparser = require('body-parser')
+var mysql = require('mysql')
+var app = express()
+
+app.use(bodyparser.urlencoded({}))
+var pool = mysql.createPool({
+	host:'127.0.0.1',
+	user:'root',
+	password:'yu62417..',
+	database:'items',
+	port:3306
+}) 
+
+//查询loupan
+app.post('/',(req,res) => {
+	res.setHeader('Access-Control-Allow-Origin','*')
+	pool.getConnection(function(err,connection){
+		if(err){
+			console.log(err)
+			return
+		}
+		var sql = `select * from loupan where state=${req.body.fenlei}`
+		connection.query(sql,function(err,data){
+			if(err){
+				console.log(err)
+				return
+		    }
+			res.send(data)
+			connection.end()
+		})
+	})
+})
+
+//查询xinwen
+app.post('/txt',(req,res) => {
+	res.setHeader('Access-Control-Allow-Origin','*')
+	pool.getConnection(function(err,connection){
+		if(err){
+			console.log(err)
+			return
+		}
+		var sql = `select * from xinwen`
+		connection.query(sql,function(err,data){
+			if(err){
+				console.log(err)
+				return
+		    }
+			res.send(data)
+			connection.end()
+		})
+	})
+})
+
+//查询详情
+app.post('/detail',(req,res) => {
+	res.setHeader('Access-Control-Allow-Origin','*')
+	pool.getConnection(function(err,connection){
+		if(err){
+			console.log(err)
+			return
+		}
+		var sql = `select * from loupan where id=${req.body.id}`
+		connection.query(sql,function(err,data){
+			if(err){
+				console.log(err)
+				return
+		    }
+			res.send(data)
+			connection.end()
+		})
+	})
+})
+
+//查询详情
+app.post('/detail2',(req,res) => {
+	res.setHeader('Access-Control-Allow-Origin','*')
+	pool.getConnection(function(err,connection){
+		if(err){
+			console.log(err)
+			return
+		}
+		var sql = `select * from xinwen where id=${req.body.id}`
+		connection.query(sql,function(err,data){
+			if(err){
+				console.log(err)
+				return
+		    }
+			res.send(data)
+			connection.end()
+		})
+	})
+})
+
+app.listen(3000,function(){
+	console.log('ok')
+})
